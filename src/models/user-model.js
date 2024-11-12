147f196775ddd91096f2af53d39c9e16d6e0639a
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-11-12 23:24:52 +0530

    added hashing password feature

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,6 +23,23 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// hashing password
+
+userSchema.pre("save", async (next) => {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = mongoose.model("user", userSchema);
 
