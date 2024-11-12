109d2955ad366f817eca3b89e4ea6ecb9504ea05
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-11-12 23:49:53 +0530

    added user signup controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,8 +1,38 @@
 const User = require("../models/user-model");
 
-const signupUser = async (req, res) => {};
+const signupUser = async (req, res) => {
+  try {
+    const { email, password, firstName, secondName } = req.body;
 
-const signinUser = async (req, res) => {};
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      return res.status(409).json({
+        message: "user already exits",
+      });
+    }
+
+    const user = await User.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      secondName: secondName,
+    });
+
+    res.status(201).json({
+      message: "signup successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const signinUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+  } catch (error) {}
+};
 
 const getCourses = async (req, res) => {};
 
